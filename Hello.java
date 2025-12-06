87ed05272db2755dfbc9fd419a9a36ac36c9e4ce
Cikal Jundi Farabi <cikaljundi60@gmail.com>
2025-12-06 22:14:04 +0700

    style: tambahkan komentar di Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Ini komentar tambahan
+        System.out.println("Hello Dunia");
     }
 }
